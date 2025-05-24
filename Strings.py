5c5fd98ca0fc82c7feb2e112bdd2db2fe49093e5
Alvin <alvinkennethmayega@gmail.com>
2025-05-24 16:20:32 +0300

    respoonse to exercise 4 on Strings

---
+++ Strings.py
@@ -0,0 +1,27 @@
+#Number 1
+name = "Alvin"
+age = 16
+id = name + str(age)
+print(id)
+
+#Number 2
+txt = " Hello, Uganda! "
+txt = txt.replace(" ", "")
+print(txt)
+
+#Number 3
+txt = txt.upper()
+print(txt)
+
+#Number 4
+txt = txt.replace("U", "V")
+print(txt)
+
+#Number 5
+y = "I am proudly Ugandan"
+range = y[1:4]
+print(range)
+
+#Number 6
+# x = "All "Data Scientists" are cool!" Gives an error
+x = "All \"Data Scientists\" are cool!"
